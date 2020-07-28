4bdcdca68f846b418722c3bb20458e44db7e6e31
Dang Luong <lsd25897@gmail.com>
2020-07-28 16:14:18 +0700

    add redux, login

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import Main from './src/Main';
 import {name as appName} from './app.json';
+
 AppRegistry.registerComponent(appName, () => Main);
---
+++ src/redux/reducers/userReducer.js
@@ -0,0 +1,27 @@
+import { userTypes } from "../types/loginType";
+
+const INITIAL_STATE = {
+    currentUser: null,
+    token: null,
+    info:null,
+	error: null,
+};
+const loginReducer = (state = INITIAL_STATE, action)=>{
+    switch(action.type){
+        case userTypes.LOGIN_SUCCESS:
+            return {
+                ...state,
+                currentUser:action.payload,
+                token: action.payload.access_token,
+                error: null,
+            }
+        case userTypes.LOGIN_FAIL:
+            return {
+                ...state,
+                error: action.payload,
+            }
+        default:
+            return state;
+    }
+}
+export default loginReducer;
